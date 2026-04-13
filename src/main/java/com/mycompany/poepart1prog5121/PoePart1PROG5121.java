7bbf7aad16c3b9c130c472a1a1244fefc9f2de99
ST10520829 <ST10520829@rcconnect.edu.za>
2026-04-13 18:56:19 +0200

    added public static void main method

--- src/main/java/com/mycompany/poepart1prog5121/PoePart1PROG5121.java
+++ src/main/java/com/mycompany/poepart1prog5121/PoePart1PROG5121.java
@@ -71,10 +71,88 @@ public class PoePart1PROG5121 {
         }
     }
     
-    
+                        
     public static void main(String[] args) {
+                
+        Scanner input = new Scanner(System.in);
+        PoePart1PROG5121 login = new PoePart1PROG5121();
         
+        System.out.println("Welcome to the Authorisation System!");
+        
+        
+        System.out.println("Register");
+        
+        System.out.println("Enter fist name:");
+        String firstName = input.nextLine();
+        
+        System.out.println("Enter last name:");
+        String lastName = input.nextLine();
+        
+        String userName;
+        while (true){
+            System.out.println("Enter userName:");
+            userName = input.nextLine();
+            
+            if (login.checkUserName(userName)){
+                break;
+                
+            }else{
+                System.out.println("Username is not correctly formatted,Please ensure that your username contains an underscore and is no more than five characters in lenght.");
+            }
+                
+        }
+            
+        String passWord;
+        while (true){
+            System.out.println("Please enter your password");
+            passWord = input.nextLine();
+
+            if (passWord.length() >= 8) {
+
+                break;
+            }else { 
+                System.out.println("The password must be at least 8 characters, Try again.");
+            }
+        }
+
+
+        String cellPhone;
+        while(true){
+            System.out.println("Enter cellphone number that contains international code(+27)");
+            cellPhone = input.nextLine();
+
+            if (login.checkCellPhoneNumber(cellPhone)){ 
+                break;
+            }else {
+                System.out.println("cell phone number incorrectly formatted. Try again.");
+            }
+        }
+        String massage = login.registerUser(userName, passWord,cellPhone, firstName, lastName);
+
+            
+        
+        //login 
+        if (massage.equals("User registerd successfully")){
+
+            System.out.println("Login");
+
+            while(true){    
+
+                System.out.println("Enter username");
+                String loginUserName = input.nextLine();
+
+                System.out.println("Enter passWord");
+                String loginUserPass = input.nextLine();
+
+                if (login.loginUser(loginUserName, loginUserPass)){
+                 //   System.out.println("Welcome " + firstName + );
+                }
+            }
+        }   
     }
+    
+    
+ 
     
 }
  
