cce13d576084face4d48c1b185c29a5dd2cd47d1
ST10520829 <ST10520829@rcconnect.edu.za>
2026-04-13 17:54:23 +0200

    intial commit

--- src/main/java/com/mycompany/poepart1prog5121/PoePart1PROG5121.java
+++ src/main/java/com/mycompany/poepart1prog5121/PoePart1PROG5121.java
@@ -2,7 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
+package com.mycompany.poepart1prog5121;
 
+/**
+ *
+ * @author brigh
+ * 
+ *
+ */
+
+import java.util.Scanner;
+
+
+public class PoePart1PROG5121 {
+    
+    public static void main(String[]args){
+        
+    }
+
+}
 
  
             
